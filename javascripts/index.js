4d14fd0698f869ebc4c02ab53d34af8f8c9b4411
majinmaru <gotch11@naver.com>
2019-01-20 02:59:00 +0900

    190120
    index pc done

---
+++ javascripts/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  
+  var slideWrapper = $(".main-slider");
+  
+  slideWrapper.slick({
+    autoplay: true,
+    autoplaySpeed:7000,
+    speed:500,
+    arrows:false,
+    dots: true,
+    swipe:true,
+    fade: true,
+    zIndex: 1,
+  });
+  
+   /*
+  document.querySelectorAll('video').forEach(function(video) {
+    video.play();
+  });
+
+  document.addEventListener("mousemove", function(event) {
+    const x = event.pageX;
+    const y = event.pageY;
+    const xCenter = window.innerWidth / 2;
+    const yCenter = window.innerHeight / 2;
+    const body = document.querySelector("body");
+    const cursor = document.querySelector("#cursor");
+    cursor.style.top = y - 24 + "px";
+    if (x < xCenter * .5) {
+      cursor.style.transform = "rotate(0deg)";
+      cursor.style.left = x + "px";
+      cursor.style.display = "block";
+      body.style.cursor = "none";
+    } else if (x > xCenter * 1.5) {
+      cursor.style.display = "block";
+      cursor.style.left = x - 48 + "px";
+      body.style.cursor = "none";
+    }
+    else {
+      cursor.style.transform = "rotate(180deg)";
+      cursor.style.display = "none";
+      body.style.cursor = "default";
+    }
+  })
+ 
+  document.addEventListener("click", function(event) {
+    const x = event.pageX;
+    const y = event.pageY;
+    const xCenter = window.innerWidth / 2;
+    const yCenter = window.innerHeight / 2;
+    if (x < xCenter * .5) {
+      $(".main-slider").slick('slickPrev');
+    } else if (x > xCenter * 1.5) {
+      $(".main-slider").slick('slickNext');
+    }
+    else {
+      return;
+    }
+  });
+  */
+});
