ec370170d4bdb4ae5896015f9107f2487087bf39
majinmaru <gotch11@naver.com>
2019-01-26 02:16:30 +0900

    190126
    index

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  
+  $(".nav-logo a").find('img').attr('src', 'images/common/fulllogo.svg');
+  var slideWrapper = $(".main-slider");
+
   function imageSetting() {
     var windowWidth = $(window).width();
     var mainSliderItem = $(".main-slider").find(".item");
@@ -12,11 +14,19 @@ $(document).ready(function(){
       $(mainSliderItem).eq(5).prepend('<div class="slider-image"><img src="images/index/mobile/slide-5.jpg"/></div>');
       $(mainSliderItem).eq(6).prepend('<div class="slider-image"><img src="images/index/mobile/slide-6.jpg"/></div>');
     }
+    slideWrapper.slick({
+      autoplay: true,
+      autoplaySpeed:7000,
+      speed:500,
+      arrows:false,
+      dots: true,
+      swipe:true,
+      fade: true,
+      zIndex: 1,
+      pauseOnHover: false
+    });
   }
   imageSetting();
-
-  $(".nav-logo a").find('img').attr('src', 'images/common/fulllogo.svg');
-  var slideWrapper = $(".main-slider");
   
   slideWrapper.slick({
     autoplay: true,
